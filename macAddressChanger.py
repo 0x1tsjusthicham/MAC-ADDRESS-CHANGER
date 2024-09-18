5014d80d7c35540eb50b81af5d7b3ec3245f75aa
XploitN1nj4 <hicham.ezzamzami@gmail.com>
2024-09-18 23:53:25 +0100

    Some improvement

--- macAddressChanger.py
+++ macAddressChanger.py
@@ -10,32 +10,44 @@ import optparse
 import re
 
 # this is a reader that will take mac address and network interface
-parser = optparse.OptionParser()
-parser.add_option("-i", "--interface", dest="network_interface", help="this place for network interfaces")
-parser.add_option("-m", "--mac", dest="new_mac", help="this place for mac addresse")
-options, arguments = parser.parse_args()
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="network_interface", help="this place for network interfaces")
+    parser.add_option("-m", "--mac", dest="new_mac", help="this place for mac addresse")
+    options, arguments = parser.parse_args()
 
-#print(options)
+    #print(options)
 
-if not options.network_interface:
-    print("[-] You forgot to put network interface")
-    exit()
-if not options.new_mac:
-    print("[-] You forgot to put new mac")
-    exit()
+    if not options.network_interface:
+        parser.error("[-] You forgot to put network interface, type -h or --help for help")
+        exit()
+    if not options.new_mac:
+        parser.error("[-] You forgot to put new mac, type -h or --help for help")
+        exit()
+
+    return options
+
 
 # they are a system commands that change mac address for network interface
-subprocess.call(f"ifconfig {options.network_interface} down", shell=True)
-subprocess.call(f"ifconfig {options.network_interface} hw ether {options.new_mac}", shell=True)
-subprocess.call(f"ifconfig {options.network_interface} up", shell=True)
+def mac_changer(network_interface, new_mac):
+    subprocess.call(f"ifconfig {network_interface} down", shell=True)
+    subprocess.call(f"ifconfig {network_interface} hw ether {new_mac}", shell=True)
+    subprocess.call(f"ifconfig {network_interface} up", shell=True)
 
-print(f"[+] Changing mac address for {options.network_interface} to {options.new_mac}")
+    print(f"[+] Changing mac address for {network_interface} to {new_mac}")
+
 
 # filtering mac address
-ifconfig_result = subprocess.check_output(f"ifconfig {options.network_interface}").decode("UTF-8")
-mac_address = re.search("\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+def get_mac(network_interface):
+    ifconfig_result = subprocess.check_output(f"ifconfig {network_interface}").decode("UTF-8")
+    mac_address = re.search("\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    return mac_address[0]
 
-if mac_address[0] == options.new_mac:
+
+options = get_arguments(network_interface, new_mac)
+mac_changer(options.network_interface, options.new_mac)
+mac_address = get_mac(options.network_interface)
+if mac_address == options.new_mac:
     print("MAC changed successfully")
 else:
     print("Something went wrong")

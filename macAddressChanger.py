aff510cc5a2e287a180ad4bd61fe87b1c6e86458
XploitN1nj4 <hicham.ezzamzami@gmail.com>
2024-09-18 23:35:24 +0100

    More dynamic

--- macAddressChanger.py
+++ macAddressChanger.py
@@ -6,13 +6,36 @@ change it manually in kali
 '''
 
 import subprocess
+import optparse
+import re
 
+# this is a reader that will take mac address and network interface
+parser = optparse.OptionParser()
+parser.add_option("-i", "--interface", dest="network_interface", help="this place for network interfaces")
+parser.add_option("-m", "--mac", dest="new_mac", help="this place for mac addresse")
+options, arguments = parser.parse_args()
 
-mac_address = "00:11:22:33:44:55"
-network_interface = "eth0"
+#print(options)
 
-subprocess.call(f"ifconfig {network_interface} down", shell=True)
-subprocess.call(f"ifconfig {network_interface} hw ether {mac_address}", shell=True)
-subprocess.call(f"ifconfig {network_interface} up", shell=True)
+if not options.network_interface:
+    print("[-] You forgot to put network interface")
+    exit()
+if not options.new_mac:
+    print("[-] You forgot to put new mac")
+    exit()
 
-print(f"[+] Changing mac address for {network_interface} to {mac_address}")
+# they are a system commands that change mac address for network interface
+subprocess.call(f"ifconfig {options.network_interface} down", shell=True)
+subprocess.call(f"ifconfig {options.network_interface} hw ether {options.new_mac}", shell=True)
+subprocess.call(f"ifconfig {options.network_interface} up", shell=True)
+
+print(f"[+] Changing mac address for {options.network_interface} to {options.new_mac}")
+
+# filtering mac address
+ifconfig_result = subprocess.check_output(f"ifconfig {options.network_interface}").decode("UTF-8")
+mac_address = re.search("\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+
+if mac_address[0] == options.new_mac:
+    print("MAC changed successfully")
+else:
+    print("Something went wrong")
